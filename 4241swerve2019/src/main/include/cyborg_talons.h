57c057a8aa18b52f5b7e31d94947fa42e837d963
Unknown <chris.herzog@gmail.com>
2019-03-02 18:05:14 -0600

    Added cyborgs_talons.h
    Common place for all Talon IDs and other Talon-specific settings to live.
    
    Modify as needed.

---
+++ 4241swerve2019/src/main/include/cyborg_talons.h
@@ -0,0 +1,43 @@
+// cyborgs_talons.h
+//
+// This is where we identify all of the talons by function
+//
+// Only use the Talon functiona name in the code - not the number!
+
+
+// drivetrain -----------------------------------------------------
+
+#define TALON_RL_DRIVE          1
+#define TALON_RL_STEER          2
+
+#define TALON_FL_DRIVE          3
+#define TALON_FL_STEER          4
+
+#define TALON_RR_DRIVE          6
+#define TALON_RR_STEER          7
+
+#define TALON_FR_STEER          8
+#define TALON_FR_DRIVE          9
+
+#define TALON_DRIVE_RAMP_TIME   0.5
+
+
+
+
+// other subsystems ------------------------------------------------
+
+#define TALON_RAMPS		5	// placeholder! - confirm!
+
+#define TALON_CLAW		10	// placeholder! - confirm!
+
+#define TALON_LIFT		11	// placeholder! - confirm!
+
+#define	TALON_ARM_EXTEND	12	// placeholder! - confirm!
+
+
+
+
+// general stuff ---------------------------------------------------
+
+#define TALON_CONFIG_TIMEOUT    30      // ms
+
