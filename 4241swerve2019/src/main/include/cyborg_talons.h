e4a7cb7137792a8ab3095937c1b415fe16fe62aa
frc4241 <36015739+frc4241@users.noreply.github.com>
2019-03-16 14:00:38 -0500

    Added Defines for encoders, updated talon Id's for practice bot

--- 4241swerve2019/src/main/include/cyborg_talons.h
+++ 4241swerve2019/src/main/include/cyborg_talons.h
@@ -7,14 +7,14 @@
 
 // drivetrain -----------------------------------------------------
 
-#define TALON_RL_DRIVE          1
+#define TALON_RL_DRIVE          3
 #define TALON_RL_STEER          2
 
-#define TALON_FL_DRIVE          3
-#define TALON_FL_STEER          4
+#define TALON_FL_DRIVE          4
+#define TALON_FL_STEER          5
 
-#define TALON_RR_DRIVE          6
-#define TALON_RR_STEER          7
+#define TALON_RR_DRIVE          7
+#define TALON_RR_STEER          6
 
 #define TALON_FR_STEER          8
 #define TALON_FR_DRIVE          9
@@ -23,10 +23,23 @@
 
 
 
+// Swerve Encoders -------------------------------------------------
+
+#define FL_ENCODER     0
+
+#define FR_ENCODER     1
+
+#define RR_ENCODER     2
+
+#define RL_ENCODER     3
+
+
+
+
 
 // other subsystems ------------------------------------------------
 
-#define TALON_RAMPS		5
+#define TALON_RAMPS		1
 
 #define TALON_LIFT		10
 
